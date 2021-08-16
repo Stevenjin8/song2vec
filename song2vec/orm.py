c0d5ff646f4bc6dd85a6350824df338f2fb0565b
Steven Jin <stevenjin8@gmail.com>
2021-08-16 18:19:30 -0400

    Create ORMs.

---
+++ song2vec/orm.py
@@ -0,0 +1,100 @@
+"""Object realtional mappings."""
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+# Intermediate table for many-to-many relationships between tracks and playlists.
+track_playlist_association = Table(
+    "association",
+    Base.metadata,
+    Column("track_uri", ForeignKey("track.uri")),
+    Column("playlist_id", ForeignKey("playlist.pid")),
+)
+
+
+class Artist(Base):
+    """Artists are users that create tracks.
+
+    Fields:
+        uri (String): unique identifier.
+        name (String): name of the artist.
+        tracks (Relationship): the tracks of this artist.
+    """
+
+    __tablename__ = "artist"
+
+    uri = Column(String, primary_key=True)
+    name = Column(String)
+    tracks = relationship("Track", back_populates="artist")
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} ID: {self.uri}, NAME: {self.name}>"
+
+
+class Track(Base):
+    """Aka a song.
+
+    Fields:
+        uri (String): unique identifier.
+        name (String): name of this track.
+        artist (Relationship): foreign key to the artist of this track. Curiously, each
+            track only has one artist.
+        album (Relationship): foreign key to the album this track belongs to.
+        playlists (Relationship): many to many with the playlists this track is in.
+    """
+
+    __tablename__ = "track"
+
+    uri = Column(String, primary_key=True)
+    name = Column(String)
+    duration = Column(Integer)
+    artist_uri = Column(String, ForeignKey("artist.uri"))
+    artist = relationship("Artist", back_populates="tracks")
+    album_uri = Column(String, ForeignKey("album.uri"))
+    album = relationship("Album", back_populates="tracks")
+
+    playlists = relationship(
+        "Playlist", secondary=track_playlist_association, back_populates="tracks"
+    )
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} URI: {self.uri}, NAME: {self.name}>"
+
+
+class Playlist(Base):
+    """A __unordered__ set of tracks.
+
+    Fields:
+        pid (Integer): unique identifier.
+        name (String): name of this playlist.
+        tracks (Relationship): many to many relationships with the tracks in this playlist.
+    """
+
+    __tablename__ = "playlist"
+
+    pid = Column(Integer, primary_key=True)
+    name = Column(String)
+    tracks = relationship(
+        "Track", secondary=track_playlist_association, back_populates="playlists"
+    )
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} ID: {self.pid}, NAME: {self.name}>"
+
+
+class Album(Base):
+    """Just what it sounds like.
+
+    Fields:
+        uri (String): unique identifier.
+        name (String): name of this album.
+        tracks (Relationship): one to many with the tracks in this album.
+    """
+
+    __tablename__ = "album"
+
+    uri = Column(String, primary_key=True)
+    name = Column(String)
+    tracks = relationship("Track", back_populates="album")
