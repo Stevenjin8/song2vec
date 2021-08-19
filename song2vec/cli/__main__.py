a33d0e08016a3cf300751d092cb2aa2d4bda1632
Steven Jin <stevenjin8@gmail.com>
2021-08-18 22:26:42 -0400

    Finish data loading

--- song2vec/cli/__main__.py
+++ song2vec/cli/__main__.py
@@ -1,19 +1,75 @@
+"""Run the cli when running the module."""
+# This is a Click convention
 # pylint: disable=import-outside-toplevel
+import multiprocessing
+
 import click
+from song2vec.cli.load import (
+    create_albums,
+    create_artists,
+    load_playlist_track_association,
+    create_playlists,
+    create_tracks,
+    load_objects,
+)
 
 
 @click.group()
 def cli():
-    pass
+    """Main cli group."""
 
 
 @click.command()
-def load_playlists():
+@click.option(
+    "--raw-data-dir",
+    type=str,
+    default="data/raw/data",
+    help="Path to folder containing the json files with the Spotify dataset.",
+)
+@click.option(
+    "--db-url",
+    type=str,
+    default="sqlite:///data/db",
+    help="URL to the database for Sqlalchemy (e.g. sqlite:///data/db)",
+)
+def load_playlists(raw_data_dir: str, db_url: str):
+    """Load the Million Playlist Dataset into a SQLite database."""
+    import sqlalchemy
+    from song2vec import models
+
     from . import load
 
-    raw_data_dir = "data/raw/data"
+    # Set up connection
+    engine = sqlalchemy.create_engine(db_url)
+    models.Base.metadata.create_all(engine)
 
-    print(load.get_slices(raw_data_dir))
+    # Create processes to read files and upload objects. Note that SQLite allows for
+    # nonexistent foreign keys, which is why we can upload everything at the same time
+    # without integrity errors.
+    filenames = load.get_slices(raw_data_dir)
+    artist_process = multiprocessing.Process(
+        target=load_objects,
+        args=(db_url, filenames, create_artists),
+    )
+    album_process = multiprocessing.Process(
+        target=load_objects, args=(db_url, filenames, create_albums)
+    )
+    track_process = multiprocessing.Process(
+        target=load_objects, args=(db_url, filenames, create_tracks)
+    )
+    playlist_process = multiprocessing.Process(
+        target=load_objects,
+        args=(db_url, filenames, create_playlists),
+    )
+    association_process = multiprocessing.Process(
+        target=load_playlist_track_association, args=(db_url, filenames)
+    )
+
+    artist_process.start()
+    album_process.start()
+    track_process.start()
+    playlist_process.start()
+    association_process.start()
 
 
 cli.add_command(load_playlists)
--- song2vec/cli/load.py
+++ song2vec/cli/load.py
@@ -1,8 +1,12 @@
-"""Functions to load data into the db."""
+"""Functions to load data into the db. I think a functional approach makes more sense
+when processing data.
+"""
+import json
 import os
-from typing import List
+from typing import Callable, Generator, Iterable, List, Set
 
 import sqlalchemy
+from tqdm import tqdm
 from song2vec import models
 
 from . import settings
@@ -17,19 +21,136 @@ def get_slices(raw_data_dir: str) -> List[str]:
     ]
 
 
-def create_artists(session: sqlalchemy.orm.Session, playlists: List[dict]) -> None:
-    """There is probably a more efficient way to do this (without loading everything
-    into memory)."""
+def create_artists(
+    playlists: List[dict], artist_uris: Set[str]
+) -> Generator[models.Artist]:
+    """Create objects representing the artists in the database.
+
+    Arguments:
+        playlists: A list of playlists as stored in the Spotify dataset.
+        artist_uris: URI of artists that have already been inserted. We need this
+            because the data is denormalized.
+
+    Yields:
+        The next new artist.
+    """
     for playlist in playlists:
         for track in playlist["tracks"]:
             uri = track["artist_uri"]
             name = track["artist_name"]
-            exists = bool(
-                session.query(models.Artist.uri)
-                .filter(models.Artist.uri == uri)
-                .count()
-            )
-            if not exists:
-                artist = models.Artist(uri=uri, name=name)
-                session.add(artist)
-    session.commit()
+            if uri not in artist_uris:
+                artist_uris.add(uri)
+                yield models.Artist(uri=uri, name=name)
+
+
+def create_albums(
+    playlists: List[dict], album_uris: Set[str]
+) -> Generator[models.Album]:
+    """Create objects representing the albums in the database.
+
+    Arguments:
+        playlists: A list of playlists as stored in the Spotify dataset.
+        album_uris: URI of albums that have already been inserted. We need this because
+            the data is denormalized.
+
+    Yields:
+        The next new album.
+    """
+    for playlist in playlists:
+        for track in playlist["tracks"]:
+            uri = track["album_uri"]
+            name = track["album_name"]
+            if uri not in album_uris:
+                album_uris.add(uri)
+                yield models.Album(uri=uri, name=name)
+
+
+def create_tracks(
+    playlists: List[dict], track_uris: Set[str]
+) -> Generator[models.Track]:
+    """Create objects representing the tracks (i.e. songs) in the database.
+
+    Arguments:
+        playlists: A list of playlists as stored in the Spotify dataset.
+        track_uris: URI of tracks that have already been inserted. We need this because
+            the data is denormalized.
+
+    Yields:
+        The next new track.
+    """
+    for playlist in playlists:
+        for track in playlist["tracks"]:
+            uri = track["track_uri"]
+            name = track["track_name"]
+            album_uri = track["album_uri"]
+            artist_uri = track["artist_uri"]
+            if uri not in track_uris:
+                track_uris.add(uri)
+                yield models.Track(
+                    uri=uri, name=name, album_uri=album_uri, artist_uri=artist_uri
+                )
+
+
+def create_playlists(playlists: List[dict], *_) -> Generator[models.Playlist]:
+    """Create objects representing playlists in the dabase. No need to worry about
+    duplicate playlists.
+
+    Arguments:
+        playlists: A list of playlists as storefd in the Spotify dataset.
+    """
+    for playlist in playlists:
+        name = playlist["name"]
+        yield models.Playlist(name=name)
+
+
+def load_objects(
+    db_url: str,
+    filenames: List[str],
+    create_objects: Callable[[List[dict], Set[str]], Iterable[models.Base]],
+) -> None:
+    """Load objects into the database. This function is mostly here to get rid of
+    boilerplate for multiprocessing.
+
+    Arguments:
+        db_url: the url of the database (e.g. "sqlite:///data/db").
+        filenames: list of file paths that contain the dataset.
+        create_objects: a callable like `create_albums`. Must take two arguments:
+            a list of playlists and a set of strings (usually ids of already-created
+            objects), and return Sqlalchemy ORM objects.
+    """
+    ids = set()
+    engine = sqlalchemy.create_engine(db_url)
+    # pylint: disable=invalid-name
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    for slice_path in tqdm(filenames):
+        session = Session()
+        with open(slice_path) as file:
+            data_slice = json.load(file)
+        session.add_all(create_objects(data_slice["playlists"], ids))
+        session.commit()
+        session.close()
+
+
+def load_playlist_track_association(db_url: str, filenames: List[str]) -> None:
+    """Load relationships between playlists and tracks to the intermediate table. We
+    have to do this a bit differently because the intermediate table is not an ORM.
+
+    Arguments:
+        db_url: the url of the database (e.g. "sqlite:///data/db").l
+        filenames: list of file paths that contain the dataset.
+    """
+    ins = models.track_playlist_association.insert()
+    engine = sqlalchemy.create_engine(db_url)
+    ins.bind = engine
+    conn = engine.connect()
+
+    for filename in tqdm(filenames):
+        with open(filename) as file:
+            data_slice = json.load(file)
+        relationships = []
+        for playlist in data_slice["playlists"]:
+            for track in playlist["tracks"]:
+                relationships.append(
+                    {"track_uri": track["track_uri"], "playlist_id": playlist["pid"]}
+                )
+        conn.execute(ins, relationships)
